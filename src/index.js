f377a8932bccacde6625e4fb1a538850fb5db4e3
xiejingyang <xjyaitf@gmail.com>
2019-08-14 23:24:08 +0800

    重新定义编辑器组件

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import PageEditorIndex from './page-editor/PageEditorIndex';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<PageEditorIndex/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
