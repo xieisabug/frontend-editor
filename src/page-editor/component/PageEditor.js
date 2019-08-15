9a9312e580cf616b85cd263fea276ad3c4869ca2
xiejingyang <xjyaitf@gmail.com>
2019-08-16 00:08:53 +0800

    稍微修改样式

--- src/page-editor/component/PageEditor.js
+++ src/page-editor/component/PageEditor.js
@@ -2,11 +2,34 @@ import * as React from 'react';
 import classNames from "classnames";
 
 export default class PageEditor extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+
+        }
+    }
+
+    onMouseDown = () => {
+
+    };
+
+    onMouseMove = () => {
+
+    };
+
+    onMouseUp = () => {
+
+    };
+
     render() {
         let containerClassName = classNames("page-editor-editor-container", {"selected-tool" : this.props.chooseType !== ""});
 
         return (
             <div className={containerClassName}>
+                <div className="page-editor-editor-page" onMouseDown={this.onMouseDown} onMouseMove={this.onMouseMove} onMouseUp={this.onMouseUp}>
+
+                </div>
             </div>
         );
     };
