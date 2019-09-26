7e15644e2b93d6c8a9b8e0248d4914028b3fba4a
xiejingyang <xjyaitf@gmail.com>
2019-09-26 12:51:42 +0800

    页面设置修改

--- src/page-editor/component/dialog/PageSettingDialog.tsx
+++ src/page-editor/component/dialog/PageSettingDialog.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Checkbox, Form, Modal} from "antd";
+import {Checkbox, Form, Modal, Tooltip} from "antd";
 import * as PropTypes from "prop-types";
 
 export default function PageSettingDialog(props: any) {
@@ -17,9 +17,7 @@ export default function PageSettingDialog(props: any) {
         <Form labelCol={{span: 6}} wrapperCol={{span: 16}}>
             <Form.Item label="是否可滚动">
                 <Checkbox checked={page.scrollable} />
-            </Form.Item>
-            <Form.Item label="是否有tab">
-                <Checkbox checked={page.isTabPage} />
+                <div className="dialog-form-item-tips">设置为不可滚动后，页面不会自动伸缩，并且页面高度所放元素会自动拉伸或者缩小</div>
             </Form.Item>
         </Form>
 
